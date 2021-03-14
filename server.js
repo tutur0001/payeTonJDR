485eb0ce305ee75656a4c22303feda7c693cffcd
tutur0001 <a.michon@it-students.fr>
2021-03-14 16:13:22 +0100

    Add draw functions in game board

--- server.js
+++ server.js
@@ -32,6 +32,34 @@ io.on('connection', socket => {
 
 });
 
+// Draw system 
+
+var line_history = [];
+
+// event-handler for new incoming connections
+io.on('connection', function (socket) {
+
+   // first send the history to the new client
+   for (var i in line_history) {
+      socket.emit('draw_line', { line: line_history[i] } );
+   }
+
+   // add handler for message type "draw_line".
+   socket.on('draw_line', function (data) {
+      // add received line to history 
+      line_history.push(data.line);
+      // send line to all clients
+      io.emit('draw_line', { line: data.line });
+   });
+});
+
+
+
+
+
+
+
+
 const PORT = 3000 || process.env.PORT;
 
 // listenner server 
